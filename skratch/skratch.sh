05e3595b1a9634f9b87ab02fd2b6228f666d0fd9
Ilias Rinis <iliasr@gmail.com>
2026-09-18 09:45:17 +0200

    skratch: refine config file usage

--- skratch/skratch.sh
+++ skratch/skratch.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-config_file="$HOME/.config/toolshed/skratch/config"
+config_file="$HOME/.config/toolshed/skratch/config.sh"
 if [[ -f "$config_file" ]]; then
   source "$config_file"
 fi
-target="${SKRATCH_TARGET:-$HOME/Syncthing/notes/opa.md}"
+: "${SKRATCH_TARGET:?SKRATCH_TARGET must be set in $config_file}"
+target="$SKRATCH_TARGET"
 
 tmpfile=$(mktemp /tmp/skratch.XXXXXX.md)
 trap 'rm -f "$tmpfile"' EXIT
